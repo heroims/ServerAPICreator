24db3d303e11b4478fe3c9d250aa0faa9c5851ae
zhaoyiqi <heroims@163.com>
2017-02-20 17:47:57 +0800

    Initial Commit

---
+++ ServerAPICreator/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  ServerAPICreator
+//
+//  Created by admin on 2017/2/20.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ ServerAPICreator/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ServerAPICreator
+//
+//  Created by admin on 2017/2/20.
+//  Copyright © 2017年 admin. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
